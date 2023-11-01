cf33dfbce50be9552f87ef50ab057d3f972ace5c
TakiMi0o0 <takimiworks@gmail.com>
2023-11-01 12:26:22 +0900

    決済処理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,26 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @item = Item.find(params[:item_id])
     @order_ship = OrderShip.new
   end
 
+  def new
+    @order_ship = OrderShip.new
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+  end
+
   def create
     @order_ship = OrderShip.new(order_params)
+    @item = Item.find(params[:item_id])
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     if @order_ship.valid?
+      pay_item
       @order_ship.save
       return redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render 'index', status: :unprocessable_entity
     end
   end
@@ -20,7 +28,18 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_ship).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+    params.require(:order_ship)
+    .permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number)
+    .merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
--- app/models/order_ship.rb
+++ app/models/order_ship.rb
@@ -1,17 +1,21 @@
 class OrderShip
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :post_code, :prefecture_id, :city, :address, :building, :phone_number
+  attr_accessor :token, :item_id, :user_id, :post_code, :prefecture_id, :city, :address, :building, :phone_number
   with_options presence: true do
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :token
+    validates :post_code, 
+      format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :item_id, :user_id, :city, :address
-    validates :phone_number, format: { with: /\A\d{10,11}\z/,message: "is invalid."}
-    
+    validates :phone_number, 
+      format: { with: /\A\d{10,11}\z/,message: "is invalid."}
   end
+  validates :prefecture_id, 
+    numericality: {other_than: 0, message: "can't be blank"}
 
   def save
-    order = Order.create(user_id: user_id)
-    Ship.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, order_id: order.id)
+    @order = Order.create(user_id: user_id)
+    Ship.create(post_code: post_code, prefecture_id: prefecture_id, city: city, 
+      address: address, building: building, phone_number: phone_number, order_id: @order.id)
   end
 
 end
