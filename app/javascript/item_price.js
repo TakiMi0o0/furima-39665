a5fbacd7bcdf69fd1afc30fc176d56bf41c42272
TakiMi0o0 <takimiworks@gmail.com>
2023-11-02 12:31:42 +0900

    【修正】エラー後の手数料JS挙動

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,9 +1,17 @@
-window.addEventListener('turbo:load', () => {
-  const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener('input', () => {
-  const addTaxDom = document.getElementById("add-tax-price");
-  addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
-  const addPofitDom = document.getElementById("profit");
-  addPofitDom.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1 ))
-  })
-});
+const price = () => {
+  if ( document.getElementById('item-price')!= null) {
+    const priceInput = document.getElementById("item-price");
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
+
+    priceInput.addEventListener("input", () => {
+      const inputValue = document.getElementById("item-price").value;
+        let tax = Math.floor(inputValue * 0.1).toLocaleString(); 
+        addTaxDom.innerHTML = tax ;
+        profitDom.innerHTML = Math.ceil(inputValue * 0.9).toLocaleString();
+    })
+  }
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
